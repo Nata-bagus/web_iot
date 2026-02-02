0a887e2edfc11f0ef962dbceb1ab99cd85a510c0
Nata-bagus <natabagusirawan@gmail.com>
2026-02-02 07:43:43 +0700

    Update cmd.js

--- api/cmd.js
+++ api/cmd.js
@@ -1,5 +1,14 @@
-import { lastCmd } from "./set";
+let lastCmd = "O";
 
 export default function handler(req, res) {
+  const { cmd } = req.query;
+
+  // kalau ada cmd → SET
+  if (cmd === "P" || cmd === "O") {
+    lastCmd = cmd;
+    return res.status(200).send("OK");
+  }
+
+  // kalau tidak ada cmd → GET
   res.status(200).send(lastCmd);
 }
